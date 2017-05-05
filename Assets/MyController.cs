f48fb9cbc2530f8ee5c7699822202c1fcb9110ef
314pies <ian121363@gmail.com>
2017-05-05 20:42:11 +0800

    Initial commit

---
+++ Assets/MyController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        character = GetComponent<Rigidbody>();
+    }
+    Rigidbody character;
+    public float MoveSpeed = 5.0f;
+    public float JumpSpeed = 300.0f;
+    public float GroundDistance=0.9f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            character.velocity = new Vector3(-MoveSpeed, character.velocity.y, character.velocity.z);//Vector3.left* MoveSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            character.velocity = new Vector3(MoveSpeed, character.velocity.y, character.velocity.z);//Vector3.right* MoveSpeed;
+        }
+        else
+        {
+            character.velocity = new Vector3(0, character.velocity.y, character.velocity.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && IsGrounded())
+        {
+            character.AddForce(Vector3.up* JumpSpeed);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, GroundDistance);
+    }
+}
